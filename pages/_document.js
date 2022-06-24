4fd0079929fc0e9895dfa5268e93380498c72b3c
drattylight <mlaux6@outlook.com>
2022-06-24 15:03:01 -0500

    Add html='en' to all pages

---
+++ pages/_document.js
@@ -0,0 +1,14 @@
+import { Html, Head, Main, NextScript } from 'next/document';
+
+// Sets up the HTMl template for Nextjs pages
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
